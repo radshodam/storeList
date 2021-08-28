33ce42f238a9a972568ca2059716335ada6c05d9
mohamadZareai <mohamadzareai@gmail.com>
2021-08-29 02:48:55 +0430

    styles nav header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ProductList from "./components/productList/productList";
-
+import styles from "./app.module.css";
 function App() {
   return (
     <div>
+      <nav>
+        <h3>shopping carts</h3>
+        <div className={styles.carts}>
+          <span>carts</span>
+          <span className={styles.number}>52</span>
+        </div>
+      </nav>
       <ProductList />
     </div>
   );
